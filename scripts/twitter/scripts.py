fabce478857c904e4166036fce03f4bc526a0deb
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-13 09:55:18 +0000

    deblog fonctions

--- scripts/twitter/scripts.py
+++ scripts/twitter/scripts.py
@@ -20,26 +20,26 @@ def generate_authorization_url(client_id, redirect_uri, state):
 client_id = "78p12y4agb23q6"
 redirect_uri = "https://hanifcode.pythonanywhere.com/"
 state = "aRandomString"
-
 auth_url = generate_authorization_url(client_id, redirect_uri, state)
 print(auth_url)
-# post_data = {
-#     "author":"urn:li:person:78r7fo4kbu8ke5",
-#     "text": "Si je vois un animal qui vole comme un cannard qui nage, comme un cannard et qui cancanne comme un cannard alors cet animale est un canard",
-#     "visibility": {
-#         "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
-#     }
-# }
 
-# headers = {
-#     "Authorization": "Bearer ********** Mon_JETON *********",
-#     "Content-Type": "application/json"
-# }
+post_data = {
+     "author":"urn:li:person:78r7fo4kbu8ke5",
+    "text": "Si je vois un animal qui vole comme un cannard qui nage, comme un cannard et qui cancanne comme un cannard alors cet animale est un canard",
+     "visibility": {
+         "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+     }
+ }
+
+headers = {
+    "Authorization": "Bearer ********** Mon_JETON *********",
+     "Content-Type": "application/json"
+ }
 
 
-# response = requests.post(endpoint, json=post_data, headers=headers)
+response = requests.post(endpoint, json=post_data, headers=headers)
 
-# if response.status_code == 201:
-#     print('POST LinkedIn crée avec succes')
-# else:
-#     print("echec de la creations du post LinkedIn ", response.text)
+if response.status_code == 201:
+    print('POST LinkedIn crée avec succes')
+else:
+    print("echec de la creations du post LinkedIn ", response.text)
